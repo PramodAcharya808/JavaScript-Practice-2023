4e520b7cb0e8e406d5a186090645ba425681937b
pramod <pramodacharya808@gmail.com>
2023-08-16 00:11:09 +0530

    12-08-23 commit

---
+++ RoughBook/002.js
@@ -0,0 +1,20 @@
+//Demonstrate Primitive Datatyp & Objects
+// N N S S B B U
+
+let a = null
+let b = 321
+let c = "Pramod"
+let d = Symbol("Im a symbole")
+let e = true
+let f = BigInt(12233455667)
+let g = undefined
+
+console.log('\n',a,'\n', b,'\n', c,'\n', d,'\n', e,'\n', f,'\n', g,'\n')
+
+let veggies = {
+    "Tomato" : 80,
+    "Onnion" : 45,
+    "Carrot" : 50,
+    "Coconut" : 15 
+};
+console.log(veggies["Tomato"])
