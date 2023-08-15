2a3ee9d97d48e0231561c13f86a90794b5611d0b
pramod <pramodacharya808@gmail.com>
2023-08-15 23:01:45 +0530

    12-08-23 commit

--- RoughBook/sumof2.js
+++
@@ -1,5 +0,0 @@
-const prompt = require('prompt-sync')();
-// const modulePath=require.resolve('D:\\PERSONAL\\Education\\Programming\\Java Script\\RoughBook\\node_modules')
-var num1 = parseFloat(prompt("Enter the first number:"));
-var num2 = parseFloat(prompt("Enter the first number:"));
-console.log("Sum of 2 numbers is "+ (num1+num2));
