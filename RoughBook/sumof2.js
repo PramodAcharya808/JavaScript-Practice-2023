bda3597ae5bf5d28a8a514a554f20abf4225b41e
pramod <pramodacharya808@gmail.com>
2023-08-15 22:45:44 +0530

    Initial commit

---
+++ RoughBook/sumof2.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')();
+// const modulePath=require.resolve('D:\\PERSONAL\\Education\\Programming\\Java Script\\RoughBook\\node_modules')
+var num1 = parseFloat(prompt("Enter the first number:"));
+var num2 = parseFloat(prompt("Enter the first number:"));
+console.log("Sum of 2 numbers is "+ (num1+num2));
---
+++ RoughBook/text.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+var first = prompt("First name: ");
+var second = prompt("Second name:");
+console.log("\nYour Full name is \n"+(first+' '+second));
