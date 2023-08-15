e28a6ce9463405ea6fc56626b90e1c1607d78e62
pramod <pramodacharya808@gmail.com>
2023-08-15 23:47:13 +0530

    12-08-23 commit

--- RoughBook/001.js
+++ RoughBook/001.js
@@ -3,19 +3,20 @@
 
 var a = 10
 let b = 20
-b = 21
 const c = 30
 
 {
     var a = 11
     console.log(a)
 
+    console.log(c)
 
     let b = 21
     console.log(b,'\n')
 
-
+    var z = 112
 }
 console.log(a)
 console.log(b)
 console.log(c)
+console.log(z)
