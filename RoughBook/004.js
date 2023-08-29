f84b45d703fb7bdb3976384de381ec4cd2e57d7f
pramod <pramodacharya808@gmail.com>
2023-08-30 00:40:28 +0530

    JS Practice

---
+++ RoughBook/004.js
@@ -0,0 +1,18 @@
+let a=8;
+let b=6;
+let c="6";
+
+//Arithmatic Operators
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a%b);
+console.log(a*b);
+console.log(a**b);
+
+// Comparision Operator
+console.log(b==c);
+console.log(b===c);
+
+//Logical Operator
+console.log(b==c || b===c);
