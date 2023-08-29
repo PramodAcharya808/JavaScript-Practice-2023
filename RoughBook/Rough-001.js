7f749e4a81215265d6a8accee709c2b376eb379c
pramod <pramodacharya808@gmail.com>
2023-08-30 00:01:42 +0530

    JS Practice

--- RoughBook/Rough-001.js
+++ RoughBook/Rough-001.js
@@ -1,1 +1,28 @@
 //Rough Practice Space
+
+let names = "ClubEpicGaming-"
+names = names+808
+
+console.log(names)
+
+console.log(typeof(names))
+
+const obj1 = {
+    student : "Pramod",
+    dept : "BCA",
+    grades : 9.8
+};
+
+obj1["gender"] = "Male"
+obj1["student"] = "Pramod Achary"
+console.log(obj1)
+
+let dictionary = {
+    pen : "Used to write",
+    book : "Used to write on it",
+    computer : "computer is an electronic machine",
+    display : "display is an output device",
+    keyboard : "Keyboard is an input device"
+};
+
+console.log(dictionary["pen"])
