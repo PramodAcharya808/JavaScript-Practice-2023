5ffe53e5851b71711a93cbf10b80ae244acdc116
pramod <pramodacharya808@gmail.com>
2023-09-02 23:34:10 +0530

    js commit

--- RoughBook/005.js
+++ RoughBook/005.js
@@ -2,3 +2,14 @@ const prompt = require('prompt-sync')();
 let a = prompt("First Name: ")
 let b = prompt("Last Name: ")
 console.log(a+" "+b);
+
+let c =prompt("Enter your age: ")
+console.log(typeof c);
+c = Number.parseInt(c)
+console.log(typeof c);
+
+if (c > 18){
+    alert("This a valid age")
+}
+
+const Swal = require('sweetalert2');
