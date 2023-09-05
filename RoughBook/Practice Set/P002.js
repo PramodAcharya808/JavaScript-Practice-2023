02ea6a35c1dd23fd1693f04870e45290abfb03d2
pramod <pramodacharya808@gmail.com>
2023-09-05 23:33:05 +0530

    js commit

--- RoughBook/Practice Set/P002.js
+++ RoughBook/Practice Set/P002.js
@@ -1,1 +1,26 @@
 //Demonstrate use of SWITCH CASE
+
+const prompt = require('prompt-sync')();
+console.log("Select any 1 fruite\n1) Apple\n2) Mango\n3) Banana\n4) Pineapple\n");
+
+let opt = Number.parseInt(prompt());
+
+switch (opt){
+    case 1:{
+        console.log(`You have selected APPLE`);
+        break;
+    }
+    case 2:{
+        console.log(`You have selected MANGO`);
+        break;
+    }
+    case 3:{
+        console.log(`You have selected BANANA`);
+        break;
+    }
+    case 4:{
+        console.log(`You have selected PINEAPPLE`);
+        break;
+    }
+    default: console.log("Incorrect selection....!");
+}
