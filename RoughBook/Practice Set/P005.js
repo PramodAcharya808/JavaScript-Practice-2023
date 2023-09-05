61346ee393978f38b3bca47bcef5f8d61b337da1
pramod <pramodacharya808@gmail.com>
2023-09-05 23:51:21 +0530

    js commit

--- RoughBook/Practice Set/P005.js
+++ RoughBook/Practice Set/P005.js
@@ -1,3 +1,5 @@
 //Display weather "YOU CAN DRIVE" or "YOU CANNOT DRIVE" based on the age which should be above 18 years. Use Ternarry Operator
 
-let age = 19
+let age = 17
+let res = (age>=18)?"You can Drive":"You cant Drive";
+console.log(res);
