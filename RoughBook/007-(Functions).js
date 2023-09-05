93e986a2e5d039551eb6fc0ab1a7100678469c9b
pramod <pramodacharya808@gmail.com>
2023-09-06 00:51:14 +0530

    js commit

--- RoughBook/007-(Functions).js
+++ RoughBook/007-(Functions).js
@@ -2,3 +2,22 @@ let a = 10
 let b = 20
 let c = 30
 
+function Avg (x, y){
+    return x + y /2; //USE Math.round(X+Y/2) TO PRINT ROUND OF VALUE
+}
+
+console.log(Avg(a,b));
+console.log(Avg(a,c));
+console.log(Avg(c,b));
+
+// Another way to create the same is function using ARROW FUNCTION
+
+console.log();
+
+let AvgNum = (x , y) =>{
+    return  x+y/2;
+}
+
+console.log(AvgNum(a,b));
+console.log(AvgNum(a,c));
+console.log(AvgNum(c,b));
