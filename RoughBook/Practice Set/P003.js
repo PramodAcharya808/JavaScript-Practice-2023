ab02d245dcc83ccc10760524b9c1df2e076645f9
pramod <pramodacharya808@gmail.com>
2023-09-05 23:38:56 +0530

    js commit

---
+++ RoughBook/Practice Set/P003.js
@@ -0,0 +1,10 @@
+//Find weather the numebr divisible by 2 and 3
+
+let a = 12
+// console.log(a%2);
+if((a%2==0) && (a%3==0)){
+    console.log("The number is divisble by both two and three")
+}
+else{
+    console.log("Number is not divisible by 2 and 3");
+}
