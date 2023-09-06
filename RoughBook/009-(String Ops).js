e7acc2c4e129b6f954cbb5d50fdce917230c281d
pramod <pramodacharya808@gmail.com>
2023-09-07 00:50:27 +0530

    js commit

---
+++ RoughBook/009-(String Ops).js
@@ -0,0 +1,11 @@
+//String Length Property 
+
+let str = "Pramod"
+console.log(str.length);
+
+// toUpperCase()
+console.log(str.toUpperCase());
+
+// toLowerCase()
+console.log(str.toLowerCase());
+
