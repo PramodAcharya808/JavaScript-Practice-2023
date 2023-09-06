d1f4826ab7aa37e96f7843be26ea938f885b6e1b
pramod <pramodacharya808@gmail.com>
2023-09-06 10:28:58 +0530

    js commit

--- RoughBook/Practice Set/P007.js
+++ RoughBook/Practice Set/P007.js
@@ -1,1 +1,12 @@
 //Print marks of student in an OBJECT using FOR IN LOOP 
+
+let marks = {
+    Pramod : 95,
+    Chandana : 96,
+    Ambika : 98,
+    Keerthi : 96
+}
+
+for (let i in marks){
+    console.log(`Marks of ${i} is ${marks[i]}`);
+}
