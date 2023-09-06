b9de8fffc1563cb79e40b36b9261ec32842987ac
pramod <pramodacharya808@gmail.com>
2023-09-06 10:59:34 +0530

    js commit

--- RoughBook/Practice Set/P008.js
+++ RoughBook/Practice Set/P008.js
@@ -4,6 +4,6 @@ const prompt = require('prompt-sync')();
 
 let num = 1
 while (num!=0){
-    num = Number.parseInt(prompt("Press 0 to exit"));
     console.log("Please Try Again !");
+    num = Number.parseInt(prompt("Press 0 to exit"));
 }
