a7294cb3318930ceab4d3860ff9328a9b41fff90
pramod <pramodacharya808@gmail.com>
2023-09-08 22:16:32 +0530

    js commit

--- RoughBook/Practice Set/P010.js
+++ RoughBook/Practice Set/P010.js
@@ -5,3 +5,14 @@ console.log("Pra\"mod".length);
 let str1 = "Hello world, this is PRAMOD";
 let str2 = str1.includes("PRAMOD");
 console.log(str2);
+
+// Startswith String function
+
+let str3 = str1.startsWith("Hello");
+console.log(str3);
+
+
+// Endswith String function
+
+let str4 = str1.endsWith("PRAMOD");
+console.log(str4);
